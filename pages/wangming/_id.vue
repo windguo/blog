5004eba3587adefe138e7449c7af6fac5005a2ec
windguo <webmaster@windguo.com>
2019-04-03 13:12:04 +0800

    1312

--- pages/wangming/_id.vue
+++ pages/wangming/_id.vue
@@ -3,6 +3,14 @@
   <div class="container">
     <h1>{{ post.title }}</h1>
     <pre>{{ post.newstime }} {{ post.diggtop }} {{ post.username }}</pre>
+    <ul>
+      <li v-for="(wangming, index) in posts" :key="index">
+        <NuxtLink :to="{ name: 'wangming-id', params: { id: wangming.id } }">
+          {{ wangming.title }}
+        </NuxtLink>
+        <div class="bottom">{{wangming.newstime}}</div>
+      </li>
+    </ul>
     <p>
       <NuxtLink to="/">
         返回首页
@@ -16,12 +24,23 @@ import axios from 'axios'
 
 export default {
   async asyncData({ params }) {
+    let [pageRes, countRes] = await Promise.all([
+      axios.get(`https://www.yishuzi.com.cn/wangming_xiaochengxu_api/?getJson=content&id=${params.id}`),
+      axios.get(`https://www.yishuzi.com.cn/wangming_xiaochengxu_api/?getJson=column&classid=0`)
+    ]);
+    console.log('pageRes',pageRes.data.result);
+    console.log('countRes',countRes.data.result);
+    
+    return {
+      post:pageRes.data.result,
+      posts:countRes.data.result
+    };return false;
     const { data } = await axios.get(`https://www.yishuzi.com.cn/wangming_xiaochengxu_api/?getJson=content&id=${params.id}`)
     return { post: data.result }
   },
   head() {
     return {
-      title: this.post.title
+      title: this.post.title + ' 网名大全 wangming.org.cn'
     }
   }
 }
